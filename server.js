d80821ca984da4ea2e1a29a19cd071f05265658a
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2020-02-15 08:33:30 +0000

    "Better.
    Much is implemented. Works pretty well and simply."

--- server.js
+++ server.js
@@ -1,30 +1,41 @@
 import path from 'path';
 import express from 'express';
+import bodyParser from 'body-parser';
+import {config, getTable} from 'stubdb';
 
 const CONSOLE_ERROR = true;
 const DEFAULT_PORT = 8667;
 const PORT = process.env.SERVEDATA_PORT || Number(process.argv[2] || DEFAULT_PORT);
 const JSON_ERROR = msg => JSON.stringify({error:msg});
 const HTML_ERROR = msg => `<h1>Error</h1><p>${msg}</p>`;
+const ROOT = path.resolve(__dirname, "serverdata-db");
+const Tables = new Map();
+
+config({root:ROOT});
 
 const Y = (req, res) => req.path + ' ' + req.route.path;
 
 const DISPATCH = {
-  'GET /form/table/:table/:id/with/:view': Y,
-  'GET /form/list/table/:table/with/:view/': Y,
-  'GET /form/list/table/:table/with/:view/order/:props': Y,
-  'GET /form/search/table/:table/with/:view': Y,
-  'GET /form/query/:query/with/:view': Y,
-  'POST /form/table/:table/new/with/:view': Y,
-  'POST /form/table/:table/:id/with/:view': Y,
-  'POST /form/action/:action/with/:view': Y,
+  'GET /form/table/:table/:id/with/:view': withView(getItem),
+  'GET /form/list/table/:table/with/:view/': withView(getList),
+  'GET /form/list/table/:table/with/:view/sort/:prop': withView(getListSorted),
+  'GET /form/search/table/:table/with/:view': withView(getSearchResult),
+  'GET /form/query/:query/with/:view': withView(getStoredQueryResult),
+  'POST /form/table/:table/new/with/:view': withView(newItem),
+  'POST /form/table/:table/:id/with/:view': withView(setItem),
+  'POST /form/action/:action/with/:view': withView(runStoredAction),
 };
 
 const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
 
 const X = (req, res) => {
   const way = `${req.method} ${req.route.path}`;
-  const result = DISPATCH[way](req, res);
+  const data = {...req.params, item: req.body, _search: req.query};
+  if ( data.table ) {
+    data.table = _getTable(data.table);
+  }
+  const result = DISPATCH[way](data);
   res.end(result);
 }
 
@@ -37,8 +48,8 @@ app.use(express.static(path.resolve(__dirname, 'public')));
 // JSON
   // getters
   app.get('/json/table/:table/:id', X);
-  app.get('/json/list/table/:table/:id/order/:props', X);
-  app.get('/json/search/table/:table/:id/order/:props', X);
+  app.get('/json/list/table/:table/:id/sort/:prop', X);
+  app.get('/json/search/table/:table/:id/sort/:prop', X);
   // auto id
   app.post('/json/table/:table', X);    
   // given id
@@ -54,7 +65,7 @@ app.use(express.static(path.resolve(__dirname, 'public')));
   // getters 
     app.get('/form/table/:table/:id/with/:view', X);
     app.get('/form/list/table/:table/with/:view/', X);
-    app.get('/form/list/table/:table/with/:view/order/:props', X);
+    app.get('/form/list/table/:table/with/:view/sort/:prop', X);
     app.get('/form/search/table/:table/with/:view', X);
   // create
   app.post('/form/table/:table/new/with/:view', X);
@@ -102,4 +113,71 @@ function Log(obj, stdErr = false) {
   if( stdErr ) {
     console.error(obj);
   }
+}
+
+function getItem({table, id}) {
+  return table.get(id);
+}
+
+function getList({table}) {
+  return table.getAll();
+}
+
+function getListSorted({table, prop}) {
+  const list = table.getAll();
+  list.sort((a,b) => {
+    if ( a[prop] < b[prop] ) {
+      return -1;
+    } else return 1;
+  });
+  return list;
+}
+
+function getSearchResult({table, _search}) {
+  const list = getList({table});
+  const keywords = _search['keywords'];
+  const result = list.filter(item => JSON.stringify(item).includes(keywords));
+  return result;
+}
+
+function getStoredQueryResult() {
+  throw new Error("Not implemented");
+}
+
+function newItem({table, item}) {
+  const id = nextKey();
+  table.put(id, item);
+  item._id = id;
+  return item;
+}
+
+function setItem({table, id, item}) {
+  console.log(item);
+  item._id = id;
+  table.put(id, item);
+  return item;
+}
+
+function runStoredAction() {
+  throw new Error("Not implemented");
+}
+
+function withView(f) {
+  return (...args) => {
+    const raw = f(...args);
+    const tempJsonView = JSON.stringify(raw);
+    return tempJsonView;
+  };
+}
+
+function _getTable(table) {
+  if ( !Tables.has(table) ) {
+    Tables.set(table, getTable(table));
+  }
+  return Tables.get(table);
+}
+
+function nextKey() {
+  const v = (Date.now()/1000) + Math.random();
+  return v.toString(36);
 }
