eb8eec09056452ec243a41d7a7c6d2ef0881dfd1
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2020-05-31 14:33:55 +0000

    "Sketching Stripe integrations re #21"

---
+++ _actions/subscribe.js
@@ -0,0 +1,96 @@
+// imports
+	import stripe from 'stripe';
+  import nodemailer from 'nodemailer';
+
+  import mailKey from '../secrets/dosycorp.com-gsuite-email-key.js';
+  import {
+    USER_TABLE, 
+    MAIL_SENDER, 
+    MAIL_HOST, MAIL_PORT,
+    LOGINLINK_TABLE, 
+  } from '../common.js';
+  import {
+    addUser, route
+  } from '../helpers.js';
+
+// consts
+  const PRICE = {
+    'Element Animal Membership': 'price_HNBnk3QEXIgjac'
+  };
+
+export default async function action({priceName}, {getTable, newItem, getSearchResult}, req, res) {
+	const stripe = (await import('stripe'))('sk_test_4Pr8nKV0ECK71hNaRG7BBdp100I1jZxzl5');
+  const priceId = PRICE[priceName];
+	const session = await stripe.checkout.sessions.create({
+		payment_method_types: ['card'],
+		line_items: [{
+			price: priceId,
+			quantity: 1,
+		}],
+		mode: 'subscription',
+		success_url: route(req, '/success?session_id={CHECKOUT_SESSION_ID}'),
+		cancel_url: route(req, '/cancel'),
+	});
+}
+
+export async function sendLoginMail({email, loginLink, req}) {
+  const id = loginLink._id;
+  const {linkHref, formAction} = newLoginLink(req, id);
+  let transporter;
+
+  try {
+    transporter = nodemailer.createTransport({
+      host: MAIL_HOST,
+      port: MAIL_PORT,
+      secure: true,
+      auth: {
+        type: 'OAuth2',
+        user: MAIL_SENDER,
+        serviceClient: mailKey.client_id,
+        privateKey: mailKey.private_key
+      }
+    });
+  } catch (e) {
+    console.warn("Error creating transport", {email, id}, e);
+    throw e;
+  }
+  //console.log("Transporter created", transporter);
+  try {
+    await transporter.verify();
+  } catch (e) {
+    console.warn("Error verifying transport", {email, id, transporter}, e);
+    throw e;
+  }
+  //console.log("Transporter verified", transporter);
+  const mail = {
+    from: MAIL_SENDER,
+    to: email,
+    subject: `Login now ${new Date}`,
+    html: `
+      <span>
+        Your 1-click login button is:
+          <form style="display:inline;" target=_blank method=POST action=${formAction}>
+            <input type=hidden name=id value=${id}>
+            <button>Login</button>
+          </form>
+      </span>
+      <br>
+        Alternately, click this <a target=_blank href=${linkHref}>link to login.</a>
+    `,
+    text: `
+      Your login link is: ${linkHref}
+    `
+  }
+  try {
+    await transporter.sendMail(mail);
+  } catch (e) {
+    console.warn("Error sending mail", {email, mail, id, transporter}, e);
+    throw e;
+  }
+  //console.log("Email sent!", {mail, email, id, transporter});
+  console.log("Yay!");
+  console.log(`Email sent: ${email}, ${JSON.stringify({mail})}`);
+  return {success: true, email, mail, id};
+}
+
+
---
+++ public/src/preCheckout.js
@@ -0,0 +1,4 @@
+import {loadStripe} from './web_modules/@stripe/stripe-js.js';
+
+const stripe = await loadStripe('pk_test_WHuL9mvNe6xYIYnpaAcwQlQi00XyMv7TDO');
+
