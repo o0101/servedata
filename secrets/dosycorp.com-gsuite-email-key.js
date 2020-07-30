e2fa5981195f7f184e86bda4c51d387e7fd87ae2
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2020-07-30 05:23:25 +0000

    "Added dosycorp key"

---
+++ secrets/dosycorp.com-gsuite-email-key.js
@@ -0,0 +1,12 @@
+export default {
+  "type": "service_account",
+  "project_id": "free-remote-browser-isolation",
+  "private_key_id": "7611a818bb23dfe5c3a32d68d39e35966c3d7e97",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCtYxmTgLKzyGZR\nHiw+UjDsOkDJS0vInPVrvbT+2EjYr9ZAo8K8NySNQ/fNd3jMDtn3VhzJ4wnmATem\n8rMPJfMhDJ/flMmUEMU0dmlESx1LtfFoHMwL1/fRWyh5/5IF8v0QXWYKafjPb8NO\no7PDr+Q4sAnvu5BVhJDJKkWQL0lT6MKYF8qE8aTecEnYyB0/7U8h6MV5LCOs6FWx\nL9kJGkjIy31XYYw6CfKdcPDafgLXj4NNtmIsPvNeRj7+mfMxcEr0GQ7NW1fRXIIS\nVmX8AGzcu/wY71OmMwtS3rTCtB8p7f0pxHxoNrDj/QOAHm4Ue9NcObQMcbQ2JZTd\ntVh+JFCbAgMBAAECggEADEKHmnBtMl0nLvEtO7LgFrP/xLv4PdUrXVTqqWJttDSq\nt6Ncr6ZOhVaa/QJHnlYmf2CtZHETUBoh70IV+yo5yMp1u39vyXJuH0mII/YTujjB\nOxVeMDAzK7CAai/yCU2nKzMPdgkMOM60Rqk4msujHYcFDNh8Wi+ndTHx/UH6s14S\nDhaGZPrsShrbTVr1J3V9PxK40Ep6jC5/Rtw1W+JnwQ3/xxoobjgm4iohWeprQWn5\ngPLClgtmTXY/+cUCtRTGZtSddNAmM0i3tP7h3Xt07hGMASgODNSESrzC8UPCdnWp\nVYJRpIHEVgxD8oAlohChC+fltqCweIPQz19WmRULkQKBgQDlJeQv1IK5pq9Q7PoK\narOlxw7MJchBGR9PJxLtFTh7iZ/rDTuUVezabwLJuCjQdOIzWL2HJDyax69oUUmD\nxqYtCWWjHSdqBEIwuBFOPzpnCGopPoo0t/HiJwrtxDmSqLE4Flg8b0Kg0x9lQYKM\nMdixHDKlJCcUZI5JkH9PlTCYXQKBgQDBtHZP6pZnPRq4mwYsVSJrj6qUudnzOXWG\n2endQK8bnMRUd1dh0/NE6VHIxmtzXIg/jcMS5MOuKyomVmqYLpoxmvup7KlSRAwe\n4CX/hTxVE0Vx6qInY6vwwNEkr6mFa+gJg3n/SqLrEA8d7RBUOnXiBva6ImLCqpKw\n4njFYPYdVwKBgCZvyavgOKFNfWDUr0hqB4JKNog12jZZMXBEfJ86DQRj3GhACvth\nwl4Neuk9M9Nrl7nHK9tsT6Ii1qBn6i3pd1xclCjdLgxRdIzZg0Wdw30e4zfR3rrj\nE/a8V8/0NzgLhdaDoP9aJlH1t/FzIKPFhLSIp1IeE1QPXfKnA0PlIYadAoGAZt5j\nhoVaYnE845TQbnK32M9vE5Svfk+hboADhpB3bf6O9Rx7t2RL6OjPmgRlw9n8MWQj\nmMEMek5KEBoMec0l7foUdGVOdnXUygDXrR8grz2Qv7KPcsOn6C894nG3YTew/nxo\nKX+Fjbf1jzr6VQDG4cY4+RiRwje7t1xOLuQTT/kCgYEAslqWFaSFOnKzgORWCtAt\nBs/NnlxrF4+moMtSsZXw4jnfUVv6KwNWcgtR51JQFKgrotWKH/ekbHWEiypgiN9g\nEHnUiDjH8CHBSvgdNdtcbpuOYsD9RlqJya5l+j1AtD/u+3vkIGqjbeIJuSn2mf7Q\nHPEG4HCQ+Wjw5JU5d6oGAcQ=\n-----END PRIVATE KEY-----\n",
+  "client_email": "payme-emails-test@free-remote-browser-isolation.iam.gserviceaccount.com",
+  "client_id": "103118625349748799041",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/payme-emails-test%40free-remote-browser-isolation.iam.gserviceaccount.com"
+}
