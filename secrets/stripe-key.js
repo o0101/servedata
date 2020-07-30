105bfac134f6da5aaa60213d4c5c6f9dc88f46a4
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2020-07-30 05:08:23 +0000

    "Added stripe key"

---
+++ secrets/stripe-key.js
@@ -0,0 +1,5 @@
+export default {
+  live: "sk_live_s6s4amirXvhg4N7IQFTQAwB200sJtV7loC",
+  test: "sk_test_4Pr8nKV0ECK71hNaRG7BBdp100I1jZxzl5"
+};
+
--- stripeKeyPublic.js
+++ stripeKeyPublic.js
@@ -1,5 +1,5 @@
 const PUBLIC = {
-  'prod': 'pk_live_ADXAdhdgLOIMVBWW5w6KYkhQ00BfqFAUeP',
-  'test': 'pk_test_WHuL9mvNe6xYIYnpaAcwQlQi00XyMv7TDO'
+  live: 'pk_live_ADXAdhdgLOIMVBWW5w6KYkhQ00BfqFAUeP',
+  test: 'pk_test_WHuL9mvNe6xYIYnpaAcwQlQi00XyMv7TDO'
 };
 export default PUBLIC;
