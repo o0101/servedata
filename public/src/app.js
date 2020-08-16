c69222408e69d3edbd69b042d93ae4a5e6912ea8
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2020-08-16 16:25:35 +0000

    "OK"

--- public/src/app.js
+++ public/src/app.js
@@ -138,7 +138,15 @@ function App(state) {
         section ${{id:'questions', class:'questions'}} ${"section"},
           h1 ${"Frequently Asked Questions"}.
             section ${{class:'block'}} ${"card"},
-              p ${"None so far!"}.
+              dl,
+                dt ${"Who can I contact if I need more info or help?"}.
+                dd,
+                  :text {"Please, "}.
+                  a ${{href:'mailto:cris@dosycorp.com?body=Hi%20Cris&subject=BrowserGap%20Help'}} 
+                    :text ${"mail me"}.
+                  :text {" for assistance."}.
+                .
+              .
             .
         .
       .
